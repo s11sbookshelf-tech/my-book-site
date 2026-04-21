04b93604a285c827249b498a06ee01182ec1aa97
s11sbookshelf-tech <s11sbookshelf@gmail.com>
2026-04-21 15:49:09 +0530

    simplest open-next config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,1 @@
-export default {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
+export default {};
